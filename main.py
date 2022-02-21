932099157116c876193fdb92a2ac7dcdefc3f67a
wuyenlin <finne0831@gmail.com>
2022-02-21 20:28:07 +0100

    added argparse in main file

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import os
 import cv2 as cv
 import matplotlib.pyplot as plt
+import argparse
 
-def ImageHist(file_name: str, DPI: int, output_dim=None):
-    img = cv.imread(file_name)
-    img_h, img_w = img.shape[0], img.shape[1]
+parser = argparse.ArgumentParser("Set Image Histogram parameters", add_help=False)
+parser.add_argument("--file_name", type=str, default='./demo.jpg')
+parser.add_argument("--dpi", type=int, default=600)
+parser.add_argument("--save_img", action="store_true")
 
-    base = os.path.basename(file_name)
-    name = os.path.splitext(base)[0]
-    ext = os.path.splitext(base)[1]
-    histb = cv.calcHist([img],[0],None,[256],[0,256])
-    histg = cv.calcHist([img],[1],None,[256],[0,256])
-    histr = cv.calcHist([img],[2],None,[256],[0,256])
+args = parser.parse_args()
+
+
+def ImageHist(args):
+    img = cv.imread(args.file_name)
+    assert img.shape[2] == 3
+
+    hist = []
+    for i in range(3):
+        hist.append( cv.calcHist(images=[img], channels=[i], mask=None, histSize=[256], ranges=[0,256]) )
+
     f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
-    
+    ax1.plot(hist[0], 'r')
+    ax2.plot(hist[1], 'g')
+    ax3.plot(hist[2], 'b')
     plt.xlabel('Pixel intensity')
-    plt.ylabel('Number of pixels',horizontalalignment='left',position=(1,1))    
-    figure = plt.gcf() # get current figure
-    ax1.fill(histr,'r');
-    ax2.fill(histg,'g');
-    ax3.fill(histb,'b');
-    if output_dim:
-        figure.set_size_inches(image_width, image_height)#in inches
-    # when saving, specify the DPI
-    new_file_name = "histogram_{}{}".format(name, ext)
-    print("\nThe filename is: {}".format(new_file_name))
-    plt.savefig(new_file_name, dpi = DPI, bbox_inches='tight')
-    plt.setp([a.get_xticklabels() for a in f.axes[:-5]], visible=True)
+    plt.ylabel('Number of pixels', horizontalalignment='left', position=(1,1))    
+
+    if args.save_img:
+        base = os.path.basename(args.file_name)
+        name, extension = base.split('.')
+        hist_file = "hist_{}.{}".format(name, extension)
+        plt.savefig(hist_file, dpi=args.dpi, format=extension, bbox_inches='tight')
+        print('Histogram saved as ./{}'.format(hist_file))
+
     plt.show()
 
 
-def main():
-    ImageHist('./demo.jpeg', 600)
 
 if __name__ == '__main__':
-    main()
+    ImageHist(args)
